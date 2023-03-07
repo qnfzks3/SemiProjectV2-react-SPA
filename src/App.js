386d342bf2e2ec90ee40fa914e7b05fb000766fa
Lea Sae Cheul <dltpcjf1005@naver.com>
2023-03-07 16:50:49 +0900

    저장소 초기화, index 페이지 작성

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import Header from "./components/layout/Header";
+import Main from "./components/pages/Main";
+import Footer from "./components/layout/Footer";
+
+
+
+
+
+//localhost: 3000/ -> Home
+//localhost: 3000/stuff -> Stuff
+//localhost: 3000/contact -> Contact
+
+
+
+function App() {
+  return (
+    <div >
+        <Header />
+        <Main />
+        <Footer />
+
+
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/pages/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+const Main=()=>{
+    return(
+        <main>
+            <img src="/img/smile.png" alt="메인이미지" />
+        </main>
+    )
+};
+
+
+export default Main;
